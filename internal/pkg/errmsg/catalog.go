890cd21bc6bcfd78c9b9d9d61fb1e0b492dcb036
yulian-art <1119253058@qq.com>
2026-09-06 20:51:52 +0800

    fix:deleteAndUpdate

--- internal/pkg/errmsg/catalog.go
+++ internal/pkg/errmsg/catalog.go
@@ -4,8 +4,9 @@ import "connectrpc.com/connect"
 
 // catalog 服务场景错误枚举。
 var (
-	ProductTemplateNotFound = Biz{connect.CodeNotFound, "商品模板不存在"}
-	StoreNotFound           = Biz{connect.CodeNotFound, "店铺不存在"}
-	BarcodeNotFound         = Biz{connect.CodeNotFound, "条码不存在"}
-	AdminOnly               = Biz{connect.CodePermissionDenied, "仅管理员可执行该操作"}
+	ProductTemplateNotFound  = Biz{connect.CodeNotFound, "商品模板不存在"}
+	StoreNotFound            = Biz{connect.CodeNotFound, "店铺不存在"}
+	BarcodeNotFound          = Biz{connect.CodeNotFound, "条码不存在"}
+	ProductTemplateForbidden = Biz{connect.CodePermissionDenied, "只能删除自己创建的模板"}
+	AdminOnly                = Biz{connect.CodePermissionDenied, "仅管理员可执行该操作"}
 )
